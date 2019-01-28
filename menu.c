67593c2bee182a88070136cf9b6cd06126fa7ee9
waldinixD8 <33494230+waldinixD8@users.noreply.github.com>
2019-01-28 17:57:25 -0300

    Update menu.c

--- menu.c
+++ menu.c
@@ -7,8 +7,8 @@ int main()
         y=0;
 	while((y!=1)&&(y!=2))
 	{
-	printf("pulse 1 si de sea aplicar cerrado\n");
-	printf("pulse 2 si de sea aplicar abierto\n");
+	printf("Pulse 1 si de sea aplicar hashing cerrado\n");
+	printf("Pulse 2 si de sea aplicar hashing abierto\n");
 	scanf("%d",&y);
 	limpiar();
 	}
